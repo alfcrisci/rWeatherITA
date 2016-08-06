c8a18f045e3bba48a8122c7aeabe43cb39427290
Alfonso <alfcrisci@gmail.com>
2016-08-06 19:17:58 +0200

    Update knots2mps.r

--- R/knots2mps.r
+++ R/knots2mps.r
@@ -3,7 +3,7 @@
 #' @description Return  mps velocity from knots value.
 #'
 #' @param val numeric: Value in inches.
-#' @param decint integer: Decimal for rounding. Default is 2.
+#' @param digits integer: Decimal for rounding. Default is 2.
 #' @param val numeric Velocity of wind in knots.
 #' @return  Return   Numeric value in meterOnsecond.
 #' @author  Istituto di Biometeorologia Firenze Italy  Alfonso crisci \email{a.crisci@@ibimet.cnr.it}.
@@ -13,10 +13,10 @@
 #'
 #' @export
 
-knots2mps <- function(val, decint=2) {
+knots2mps <- function(val, digits=2) {
   
   val_new <- val * 0.514444
-  val_new <- round(val_new, decint)
+  val_new <- round(val_new, digits)
   
   return(val_new)
 }
