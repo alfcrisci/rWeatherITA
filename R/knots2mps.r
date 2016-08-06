0e289d3ab2386e9ecac88b5f76678dfcd930bf8b
Alfonso <alfcrisci@gmail.com>
2016-08-06 19:19:26 +0200

    Update knots2mps.r

--- R/knots2mps.r
+++ R/knots2mps.r
@@ -4,7 +4,6 @@
 #'
 #' @param val numeric: Value in inches.
 #' @param digits integer: Decimal for rounding. Default is 2.
-#' @param val numeric Velocity of wind in knots.
 #' @return  Return   Numeric value in meterOnsecond.
 #' @author  Istituto di Biometeorologia Firenze Italy  Alfonso crisci \email{a.crisci@@ibimet.cnr.it}.
 #' @keywords GSOD data
